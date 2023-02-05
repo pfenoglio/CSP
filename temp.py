2afc4c0160d077f68d272ede5d8f7ec5a6bd714c
pfenoglio <54938878+pfenoglio@users.noreply.github.com>
2023-02-05 14:49:10 -0600

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,20 @@
+# This code has 5 syntax errors! Find them and correct them.
+# When you have found and corrected all five, run the program
+# and verify that it works. You can change the initial score
+# to test different outcomes of the program.
+
+# Create a variable called score, setting it to 10
+score = 10
+
+# If the score is zero or less
+if scor <= 0:
+  # Output to the user that they lose
+  print("You Lose!)
+else
+  # Output - You Win! - and the score to the user
+  print("You Win!")
+  prnit("Your score is: ")
+  print(score)
+
+  # Wish the user good luck next time (If they win AND if they lose)
+  print("Good luck next time.")
